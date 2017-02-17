862aaaa96331aea80eea7ec55c5d6013ba0d1e9b
LuisLe2207 <khoa.led2207@gmail.com>
2017-02-17 10:19:44 +0700

    Fixed SQL injection Server.php

--- JS/JS_Form_Ajax/Server/server.php
+++ JS/JS_Form_Ajax/Server/server.php
@@ -1,26 +1,25 @@
 <?php header("Access-Control-Allow-Origin: *"); ?>
 <?php
-	$username = $_POST['username'];
-	
-	//	mysqli("servername", "username", "password", "dbname"); 
-	$conn = new mysqli("mysql.hostinger.vn", "u257267306_test", "gmorunsystemphp", "u257267306_test");
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	} else {
-		 
-		$get_acc_sql = "SELECT username FROM accounts WHERE username = '$username'";
-		$result = $conn->query($get_acc_sql);
-
-		if ($result->num_rows > 0) {
-			echo "Failed! Username exist!";
+	if (isset($_POST['username'])) {
+		$username = $_POST['username'];
+		$conn = new mysqli("mysql.hostinger.vn", "u257267306_test", "gmorunsystemphp", "u257267306_test");
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
 		} else {
-			$sql = "INSERT INTO accounts (username) VALUES ('$username')";
-			if ($conn->query($sql) === TRUE) {
-				echo "Complete";
+			$selectQuery = sprintf("SELECT username FROM accounts WHERE username = '%s' ", mysqli_real_escape_string($conn, $username));
+			$result = $conn->query($selectQuery);
+
+			if ($result->num_rows > 0) {
+				echo "Failed! Username exist!";
 			} else {
-			echo "Failed!";
+				$insertQuery = sprintf("INSERT INTO accounts (username) VALUES ('%s')", mysqli_real_escape_string($conn, $username));
+				if ($conn->query($insertQuery)) {
+					echo "Complete";
+				} else {
+				echo "Failed!";
+				}
+				$conn->close();
 			}
-			$conn->close();
 		}
-	}	
+	}
 ?>
