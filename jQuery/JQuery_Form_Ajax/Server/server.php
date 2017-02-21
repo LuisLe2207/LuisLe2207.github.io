436a893740fbf51ce1cddce1fd19d141e513c2c7
LuisLe2207 <khoa.led2207@gmail.com>
2017-02-21 21:32:54 +0700

    Add comment code. Merge voi develop

--- jQuery/JQuery_Form_Ajax/Server/server.php
+++ jQuery/JQuery_Form_Ajax/Server/server.php
@@ -1,29 +1,30 @@
-
+<?php header("Access-Control-Allow-Origin: *"); ?>
 <?php
-	if (isset($_POST['username'])) {
+if (isset($_POST['username'])) {
 		$username = $_POST['username'];
-		//	mysqli("servername", "username", "password", "dbname"); 
-		$conn = new mysqli("localhost", "root", "", "form_ajax");
+		// create connection
+		$conn = new mysqli("mysql.hostinger.vn", "u257267306_test", "gmorunsystemphp", "u257267306_test");
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		} else {
-			 
-			$get_acc_sql = "SELECT username FROM accounts WHERE username = '$username'";
-			$result = $conn->query($get_acc_sql);
-
+			// Validate param. Prevent from sql injection
+			// Check username if existe
+			$selectQuery = sprintf("SELECT username FROM accounts WHERE username = '%s' ", mysqli_real_escape_string($conn, $username));
+			$result = $conn->query($selectQuery);
+			// If exist
 			if ($result->num_rows > 0) {
 				echo "Failed! Username exist!";
 			} else {
-				$sql = "INSERT INTO accounts (username) VALUES ('$username')";
-				if ($conn->query($sql) === TRUE) {
+				// Validate param. Prevent from sql injection
+				// Insert into database if not exist
+				$insertQuery = sprintf("INSERT INTO accounts (username) VALUES ('%s')", mysqli_real_escape_string($conn, $username));
+				if ($conn->query($insertQuery)) {
 					echo "Complete";
 				} else {
 				echo "Failed!";
 				}
 				$conn->close();
 			}
-		}	
-	} else {
-		echo "Falied!";
+		}
 	}
 ?>
